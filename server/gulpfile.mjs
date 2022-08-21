13cb4f47f444fdadff0ba4c231bd908a1c38b926
Adam Dunsmoor <adamdunsmoor@pm.me>
2022-08-20 22:18:53 -0500

    add gulp test

--- server/gulpfile.mjs
+++ server/gulpfile.mjs
@@ -2,14 +2,22 @@
 import gulp from 'gulp';
 import { deleteAsync } from 'del';
 import ts from "gulp-typescript";
+import nodemon from "gulp-nodemon";
 
 // config
 var tsProject = ts.createProject("tsconfig.json");
 var filesToMove = [
   "./src/serverConfig.json",
 ];
+var logFile = "C://Users//adamd//Documents//GitHub//ng-openvpn-status//server//log//test.log";
 
 // Tasks
+gulp.task("clean-log", function () {
+  return deleteAsync([
+    logFile
+  ]);
+});
+
 gulp.task("clean-build", function () {
   return deleteAsync(["./dist"]);
 });
@@ -25,5 +33,15 @@ gulp.task("build-ts", function () {
     .js.pipe(gulp.dest("./dist"));
 });
 
+gulp.task("serve", function () {
+  gulp.watch([
+    "./**/*.ts",
+    "./**/*.config",
+  ]);
+
+  nodemon("nodemon.json");
+});
+
 // Combined tasks
+gulp.task("test", gulp.series("clean-log", "serve"));
 gulp.task("default", gulp.series("clean-build", "move", "build-ts"));
