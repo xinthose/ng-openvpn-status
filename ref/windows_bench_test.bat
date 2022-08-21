3ac5ceea08ff68f355310c61d520ed9cd7412023
Adam Dunsmoor <adamdunsmoor@pm.me>
2022-08-20 22:38:00 -0500

    update bat file

--- ref/windows_bench_test.bat
+++ ref/windows_bench_test.bat
@@ -5,10 +5,10 @@ setlocal
 cd C:\Users\adamd\Documents\GitHub\ng-openvpn-status
 
 @REM client
-START C:\Windows\System32\cmd.exe /k "cd client && cd && ng serve"
+START C:\Windows\System32\cmd.exe /k "cd client && ng serve"
 
 @REM server
-START C:\Windows\System32\cmd.exe /k "cd server && gulp && node dist/app.js"
+START C:\Windows\System32\cmd.exe /k "cd server && gulp test"
 
 endlocal
 
